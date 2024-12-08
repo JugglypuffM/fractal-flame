e32180d975443d0861b2e06ded31fd73203d686c
JugglypuffM <stas.klinin@gmail.com>
2024-12-08 19:05:58 +0500

    feat: определена доменная модель конфигурации приложения

---
+++ src/main/scala/domain/console/Config.scala
@@ -0,0 +1,18 @@
+package domain.console
+
+import domain.image.Color
+import domain.transforms.{Affine, Transform, Variation}
+import fs2.io.file.Path
+
+case class Config(
+    iterations: Int = 1000000,
+    threads: Int = 1,
+    width: Int = 1920,
+    height: Int = 1080,
+    xMin: Double = -1.777,
+    xMax: Double = 1.777,
+    yMin: Double = -1,
+    yMax: Double = 1,
+    transforms: List[Transform] = List.empty,
+    filePath: Path = Path("./image.png")
+)
