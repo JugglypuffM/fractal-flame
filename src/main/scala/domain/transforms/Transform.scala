15a6be35a7c2c891adc4d91682309c03eda6ff22
JugglypuffM <stas.klinin@gmail.com>
2024-12-08 19:02:19 +0500

    feat: определена доменная модель полной трансформации

---
+++ src/main/scala/domain/transforms/Transform.scala
@@ -0,0 +1,8 @@
+package domain.transforms
+
+import cats.Applicative
+import cats.implicits.catsSyntaxApplicativeId
+
+case class Transform (affine: Affine, variation: Variation){
+  def apply[F[_]: Applicative](point: Point): F[Point] = variation(affine(point)).pure[F]
+}
